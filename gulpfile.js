101fd65bb67180fc71a1d18f3fae3a5ccb78f78c
Sally <sallychen426@gmail.com>
2017-02-15 13:18:59 -0500

    2/15 updated

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp'),
+watch = require ('gulp-watch');
+
+gulp.task('styles',function(){
+	return gulp.src('./app/styles/styles.css').pipe(gulp.dest('./app/temp/styles'));
+});
+
+gulp.task('watch',function(){
+	
+	watch('./index.html',function(){
+		gulp.start('html');
+	});
+	watch('./app/styles/**/*.css',function(){
+		gulp.start('styles');
+	});
+})
